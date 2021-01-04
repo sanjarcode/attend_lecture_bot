07a7d968b2059be698ac4191ab94422b2e8a60a1
sanjarcode <sanjarcode@gmail.com>
2021-01-04 12:36:56 +0530

    Add subject name to logs

--- attend_lecture.py
+++ attend_lecture.py
@@ -2,15 +2,16 @@
 from attend_module import attend_meet, log_attendance
 import sys
 
-if __name__ == '__main__' and len(sys.argv) == 5:
-	username = sys.argv[1]
-	password = sys.argv[2]
-	meet_url = sys.argv[3]
-	lecture_time = sys.argv[4]
+def attend_lecture():
+	username, password, meet_url, lecture_time, subject_name = sys.argv[1:6]
 	try:
 		if(attend_meet(username, password, meet_url, lecture_time)):
-			log_attendance(True)  # no cause required
+			log_attendance(True, subject_name)  # no cause required
 		else:
-			log_attendance(False, 'Invalid config file')
+			log_attendance(False, subject_name, 'Invalid config file')
 	except Exception as e:
-		log_attendance(False, str(e))
+		log_attendance(False, subject_name, str(e))
+
+if __name__ == '__main__':
+	if len(sys.argv) == 6:
+		attend_lecture()
